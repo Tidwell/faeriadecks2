0779c0608338d3a5540218eb231e549aac45fddb
Aaron Tidwell <Aaron.Tidwell@gmail.com>
2016-04-08 20:12:52 -0400

    switch nav to side, fix some mobile issues

--- app/scripts/controllers/root.js
+++ app/scripts/controllers/root.js
@@ -8,6 +8,47 @@
  * Controller of the faeriadecks2App
  */
 angular.module('faeriadecks2App')
-	.controller('RootCtrl', function(User) {
+	.controller('RootCtrl', function(User, $timeout, $mdSidenav) {
 		this.user = User.get();
+		var vm = this;
+		this.toggleLeft = buildDelayedToggler('left');
+
+		this.close = function() {
+			console.log('close')
+			vm.toggleLeft();
+			
+		};
+
+		function debounce(func, wait, context) {
+	      var timer;
+	      return function debounced() {
+	        var context = vm,
+	            args = Array.prototype.slice.call(arguments);
+	        $timeout.cancel(timer);
+	        timer = $timeout(function() {
+	          timer = undefined;
+	          func.apply(context, args);
+	        }, wait || 10);
+	      };
+	    }
+	    /**
+	     * Build handler to open/close a SideNav; when animation finishes
+	     * report completion in console
+	     */
+	    function buildDelayedToggler(navID) {
+	      return debounce(function() {
+	        $mdSidenav(navID)
+	          .toggle()
+	          .then(function () {
+	          });
+	      }, 200);
+	    }
+	    function buildToggler(navID) {
+	      return function() {
+	        $mdSidenav(navID)
+	          .toggle()
+	          .then(function () {
+	          });
+	      }
+	    }
 	});
